2d4bc961eff637abe98a89f2d5ed4cc31afc35f9
Moz <39520581+moz-ljp@users.noreply.github.com>
2019-04-16 23:59:48 +0100

    Newer version of file
    + added new creation method of startup.scd file that doesn't require downloading (but if it fails it will execute the old method)
    + added auto install for GIT
    + changed some conditions so supercollider won't try to install unless you have GIT (as it's required to install FoxDot Quark)
    + added update checking for FoxDot
    + added checking to see if a program is already installed (so if supercollider or git are already installed it won't do it again)
    + added auto creation of a startup file for foxdot (contains opening SuperCollider and then FoxDot)
    + some minor presentation changes

--- FoxDot installer.py
+++ FoxDot installer.py
@@ -1,6 +1,14 @@
 print("Importing dependencies")
 
-import time
+try:
+    import time
+except:
+    print("Couldn't import time library")
+
+try:
+    import shutil
+except:
+    print("Couldn't import shutil")
 
 try:
     import os
@@ -32,24 +40,17 @@ try:
 except:
     print("Couldn't import getpass library")
 
-try:
-    import pyperclip
-except:
-    pyperClipFound = input("Pyperclip library not found - install? (y/n)")
-    pyperClipFound.lower()
-    if(pyperClipFound == "y"):
-        os.system("pip install pyperclip")
-        import pyperclip
-    else:
-        print("Pyperclip not installed")
-
 superColliderURL = "https://github.com/supercollider/supercollider/releases/download/Version-3.10.2/SuperCollider-3.10.2-Windows-x64-VS.exe"
 
 scdURL = "https://github.com/moz-ljp/foxdotautoinstaller/releases/download/v0.3/startup.scd"
 
+gitURL = "https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/Git-2.21.0-64-bit.exe"
+
 superColliderFoxQuark = 'Quarks.install("FoxDot")'
 
 foxInstalled = False
+
+gitInstalled = False
 
 username = getpass.getuser()
 
@@ -67,13 +68,19 @@ def after():
     print("FoxDot is now installed and should be fully functional")
 
     presGap()
+
+    createStartup = input("Would you like to make a startup file for foxdot? (y/n): ")
+    createStartup = createStartup.lower()
+
+    if(createStartup == "y"):
+        makeStartup()
+
+    presGap()
     
     website = input("Do you want to visit their website (y/n): ")
     website = website.lower()
     if(website == "y"):
         os.system("start https://foxdot.org")
-
-    print("Thankyou for using this tool")
 
     presGap()
     
@@ -81,6 +88,35 @@ def after():
     myWeb = myWeb.lower()
     if(myWeb == "y"):
         os.system("start http://moz-programs.ml/home.html")
+
+    mygithub = input("Would you like to visit my website? (y/n): ")
+    mygithub = mygithub.lower()
+    if(mygithub == "y"):
+        os.system("start https://github.com/moz-ljp/foxdotautoinstaller/")
+
+    print("Thankyou for using this tool")
+
+def makeStartup():
+
+    with open("FoxDot-Startup.py", "w") as file:
+            file.write('import time\nimport os \nos.system("start scide.exe")\ntime.sleep(10)\nos.system("python -m FoxDot")')
+
+    username = getpass.getuser()
+
+    downloadDesktop = '/Users/'+username+'/Desktop/'
+
+    
+    print("Your startup file will be created")
+    print("Where would you like your startup file installed")
+    print("To enter an exact path, type the PATH (C:/Users/whatever")
+    print("To install at desktop, type DESKTOP")
+    location = input("")
+
+    if(location != "DESKTOP"):
+        shutil.move("FoxDot-Startup.py", location+"/FoxDot-Startup.py")
+    elif(location == "DESKTOP"):
+        shutil.move("FoxDot-Startup.py", "C:"+downloadDesktop+"/FoxDot-Startup.py")
+        
 
 presGap()
 
@@ -105,7 +141,6 @@ if(platform.system() == "Windows"):
     try:
         presGap()
         print("Installing FoxDot python library")
-        presGap()
         installFox = subprocess.Popen("pip install FoxDot", shell=True, stdout=subprocess.PIPE).stdout
         installDetails = installFox.read()
         presGap()
@@ -129,24 +164,57 @@ if(platform.system() == "Windows"):
         if(getPip == "y"):
             os.system("python get-pip.py")
 
-if(foxInstalled == True):
-    try:
-        print("Installing SuperCollider")
-        urllib.request.urlretrieve(superColliderURL, downloadDesktop+'/SuperCollider-Windows-x64-VS.exe')
-        presGap()
-        print("Please run through the SuperCollider setup")
-        presGap()
-        os.system(downloadDesktop+'/SuperCollider-Windows-x64-VS.exe')
-        presGap()
-        os.remove(downloadDesktop+'/SuperCollider-Windows-x64-VS.exe')
-        urllib.request.urlretrieve(scdURL, 'C:/Users/'+username+'/AppData/Local/SuperCollider/startup.scd')
-        os.system("start scide.exe")
-        presGap()
-        time.sleep(10)
-        print("Starting FoxDot")
-        procTwo = subprocess.Popen("python -m FoxDot", shell=True)
-        after()
-    except:
-        print("Something went wrong trying to install SuperCollider")
+    if(foxInstalled == True):
+        try:
+            null = open("/dev/null", "w")
+            subprocess.Popen("git", stdout=null, stderr=null)
+            null.close()
+            print("GIT already installed")
+            gitInstalled = True
+
+        except OSError:
+            print("Installing GIT")
+            presGap()
+            urllib.request.urlretrieve(gitURL, downloadDesktop+'/Git-64.exe')
+            print("Please run through the GIT setup")
+            os.system(downloadDesktop+'/Git-64.exe')
+            os.remove(downloadDesktop+'/Git-64.exe')
+            presGap()
+            gitInstalled = True
+
+    if(gitInstalled == True):
+        try:
+            try:
+                null = open("/dev/null", "w")
+                subprocess.Popen("git", stdout=null, stderr=null)
+                null.close()
+                print("SuperCollider already installed")
+
+            except OSError:      
+                print("Installing SuperCollider")
+                urllib.request.urlretrieve(superColliderURL, downloadDesktop+'/SuperCollider-Windows-x64-VS.exe')
+                presGap()
+                print("Please run through the SuperCollider setup")
+                presGap()
+                os.system(downloadDesktop+'/SuperCollider-Windows-x64-VS.exe')
+                presGap()
+                os.remove(downloadDesktop+'/SuperCollider-Windows-x64-VS.exe')
+                
+            with open("startup.scd", "w") as file:
+                file.write('Quarks.install("FoxDot"); FoxDot.start')
+            try:
+                shutil.move("startup.scd", "C:/Users/"+username+"/AppData/Local/SuperCollider/startup.scd")
+            except:
+                print("Something went wrong moving the scd file")
+                print("Using alternative method")
+                urllib.request.urlretrieve(scdURL, 'C:/Users/'+username+'/AppData/Local/SuperCollider/startup.scd')
+            os.system("start scide.exe")
+            presGap()
+            time.sleep(10)
+            print("Starting FoxDot")
+            procTwo = subprocess.Popen("python -m FoxDot", shell=True)
+            after()
+        except:
+            print("Something went wrong trying to install SuperCollider")
 
 input("")
